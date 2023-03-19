5cfe2d0c52e3830740de49c55332126f832c6290
Nanakusa Mori <128291452+NanakusaMori@users.noreply.github.com>
2023-03-19 20:11:12 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://NanakusaMori.github.io/FreeChatGPT',
+  base: 'ssh://NanakusaMori.github.io/FreeChatGPT',
   plugins: [react()],
   resolve: {
     alias: {
